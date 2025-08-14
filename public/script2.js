55b006343f5c59c895696cef37111c1aa779fd7d
nrmhombre <b2029nrmhombre@pshs.edu.ph>
2025-08-14 10:38:29 +0800

    3rd Non Graded

--- public/script2.js
+++ public/script2.js
@@ -1,13 +1,10 @@
-if (sure) {
-    let totalRequired = (Number(numPerDay) * 365) * (Number(maxAge)) - Number((age));
+function func1() {
 
-    document.write("You will need " + totalRequired + " " + snackType + " to last you until the ripe old age of " + maxAge);
-    alert(`You will need ${totalRequired} ${snackType} to last you until the ripe old age of ${maxAge}`);
-    console.log("You will need " + totalRequired + " " + snackType + " to last you until the ripe old age of " + maxAge);
+let age = document.getElementById("age").value;
+let maxAge = document.getElementById("maxAge").value;
+let numPerDay = document.getElementById("numPerDay").value;
+let snackType = document.getElementById("snackType").value;
+let totalRequired = (Number(numPerDay) * 365) * (Number(maxAge) - Number(age));
+document.getElementById("resultHere2").innerHTML = `You will need ${totalRequired} ${snackType} to last you until the ripe old age of ${maxAge}`;
 
 }
-
-else {
-    window.alert("Please click again.");
-}
-    
